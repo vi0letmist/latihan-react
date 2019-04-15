ce0dcb70055e716487ef18af97234acf0e573507
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-04-15 10:34:35 +0700

    add materialize & menggunakan grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Product from "./Product";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="row">
-        <header className="App-header">
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="row">
+                {/*<header className="App-header">*/}
+                <Product judul="Power Bank" harga="10.0000"/>
+                <Product judul="Power Bank" harga="10.0000"/>
+                <Product judul="Power Bank" harga="10.0000"/>
+                <Product judul="Power Bank" harga="10.0000"/>
+                {/*</header>*/}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg'
 import './App.css'
 
-class Product extends Component{
-    render(){
+class Product extends Component {
+    render() {
         return (
-            <div className={this.props.className}>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>{this.props.judul}</h1>
-                <h3>Harga : Rp. {this.props.harga}</h3>
+            <div className="col s6 m4 l3">
+                <div className="card blue-grey darken-1">
+                    <div className="card-image white-text">
+                        <img src={logo} className=""/>
+                    </div>
+                    <div className="card-content white-text">
+                        <h3 className="card-title">{this.props.judul}</h3>
+                        <h4>Rp. {this.props.harga}</h4>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
             </div>
         );
     }
