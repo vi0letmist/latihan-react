d0341720a19179745d17fa44f09353c6619a7187
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-04-16 13:28:19 +0700

    set state and handle submit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,80 @@ import './App.css';
 import Product from "./Product";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        let api_url = 'https://my-json-server.typicode.com/techtona/product_api2/products';
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({products: data});
+            })
+            .catch(function (e) {
+                console.log(e);
+            });
+    }
+
+    hapusBaris = (key) => {
+        this.state.products.splice(key,1);
+        this.setState({products : this.state.products});
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        let judul = this.refs.nama.value;
+        let harga = this.refs.harga.value;
+        let id = this.refs.id.value;
+
+        // console.log(nama,harga,id);
+        this.state.products.push({id,judul,harga});
+        this.setState({mahasiswa: this.state.mahasiswa});
+
+        this.refs.form.reset();
+    }
+
     render() {
         return (
             <div className="row">
-                {/*<header className="App-header">*/}
-                <Product judul="Power Bank" harga="10.0000"/>
-                <Product judul="Power Bank" harga="10.0000"/>
-                <Product judul="Power Bank" harga="10.0000"/>
-                <Product judul="Power Bank" harga="10.0000"/>
-                {/*</header>*/}
+                <div className="col l4">
+                    <form ref="form" onSubmit={this.handleSubmit}>
+                        <input type="text" ref="id" placeholder="ID Produk"/>
+                        <input type="text" ref="nama" placeholder="Nama Produk"/>
+                        <input type="text" ref="harga" placeholder="Harga"/>
+                        <button type="submit" className="btn waves-effect">Simpan</button>
+                    </form>
+                </div>
+                <div className="col l8">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Nama Produk</th>
+                            <th>Harga</th>
+                            <th>Pilihan</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.products.map((data, key) => {
+                            return (
+                                <tr key={data.id}>
+                                    <td>{data.judul}</td>
+                                    <td>{data.harga}</td>
+                                    <td>
+                                        <button onClick={()=>this.hapusBaris(key)} className="waves-effect waves-teal btn-small red">Hapus</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+                
             </div>
         );
     }
--- src/Product.js
+++ src/Product.js
@@ -8,14 +8,14 @@ class Product extends Component {
             <div className="col s6 m4 l3">
                 <div className="card blue-grey darken-1">
                     <div className="card-image white-text">
-                        <img src={logo} className=""/>
+                        <img src={logo} className="img-responsive"/>
                     </div>
                     <div className="card-content white-text">
-                        <h3 className="card-title">{this.props.judul}</h3>
+                        <h3 className="card-title"><strong>{this.props.judul}</strong></h3>
                         <h4>Rp. {this.props.harga}</h4>
                     </div>
                     <div className="card-action">
-                        <a href="#">This is a link</a>
+                        <a href="#">Hapus</a>
                     </div>
                 </div>
             </div>
