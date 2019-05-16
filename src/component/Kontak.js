ed8617e2cacee593a4d1e856f2a9022bce8872ce
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2019-05-16 07:51:05 +0700

    add kontak

--- src/component/Kontak.js
+++ src/component/Kontak.js
@@ -11,9 +11,40 @@ class Kontak extends Component {
     render() {
         return (
             <body>
-            <div>
-                <h3 className="center">KONTAK</h3>
-            </div>
+                <div className="container">
+                    <div class="row">
+                        <div class="col s12 welcome">
+                                <div id="1" class="col s12">
+                                <h1 align="center">Welcome to<br></br>this stupid<br></br>ugly web</h1>
+                                </div>
+                                <div id="2" class="col s12">Test 2</div>
+                                <div id="3" class="col s12">Test 3</div>
+                                <div id="4" class="col s12">Test 4</div>
+                                <div id="5" class="col s12">Test 5</div>
+                                <div id="6" class="col s12">Test 6</div>
+                                <div id="7" class="col s12">Test 7</div>
+                                <div id="8" class="col s12">Test 8</div>
+                                <div id="9" class="col s12">Test 9</div>
+                                <div id="10" class="col s12">Test 10</div>
+                                <div id="11" class="col s12">Test 11</div>
+                                <div id="12" class="col s12">Test 12</div>
+                            <ul class="tabs transparent numbs">
+                                <li class="tab col s1"><a className="active" href="#1">1</a></li>
+                                <li class="tab col s1"><a href="#2" style="color: white;">2</a></li>
+                                <li class="tab col s1"><a href="#3">3</a></li>
+                                <li class="tab col s1"><a href="#4">4</a></li>
+                                <li class="tab col s1"><a href="#5">5</a></li>
+                                <li class="tab col s1"><a href="#6">6</a></li>
+                                <li class="tab col s1"><a href="#7">7</a></li>
+                                <li class="tab col s1"><a href="#8">8</a></li>
+                                <li class="tab col s1"><a href="#9">9</a></li>
+                                <li class="tab col s1"><a href="#10">10</a></li>
+                                <li class="tab col s1"><a href="#11">11</a></li>
+                                <li class="tab col s1"><a href="#12">12</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
             </body>
         );
     }
--- src/component/Portofolio.js
+++ src/component/Portofolio.js
@@ -1,40 +1,57 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import ava from '../Images/1.jpg';
 import CardProduct from "./CardProduct";
 
-class Portofolio extends Component {
-    constructor(){
-        super();
-        this.state = {
-            products : []
-        };
-    }
-
-    componentDidMount() {
-        fetch('http://my-json-server.typicode.com/techtona/product_api/product')
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({
-                    products : data
-                })
-            })
-            .catch((m) => {
-                console.log("Error : "+m);
-            })
-    }
-
-    render() {
+class Portofolio extends React.Component {
+    state = {
+        persons: []
+      }
+    
+      componentDidMount() {
+        axios.get(`https://my-json-server.typicode.com/vi0letmist/latihan_pwa_api/mahasiswa`)
+          .then(res => {
+            const persons = res.data;
+            this.setState({ persons });
+          })
+      }
+    
+      render() {
         return (
-            <div>
-                <h2>Halaman Portofolio</h2>
-                <br/>
-                {this.state.products.map((data) => {
-                    return(
-                        <CardProduct title={data.nama} price={data.harga}/>
-                    )
-                })}
+            <body>
+            <div className="container">
+            <div className="row">
+            <div className="col s12 m12 l12 welcome blue-grey lighten-5 z-depth-3">
+            
+      
+      <ul>
+            { this.state.persons.map(person =>
+                <div class="col s12 m4 l4">
+                    <div class="card-panel grey lighten-5 z-depth-1">
+                        <div class="row valign-wrapper">
+                            <div class="col s2">
+                                <img src={ava} alt="" class="circle" width="40px"></img>
+                            </div>
+                        <div class="col s10">
+                            <span class="black-text">
+                                {person.name}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>)}
+          </ul>
+            
+            {/* <ul>
+            { this.state.persons.map(person => <li>{person.name}</li>)}
+          </ul> */}
+          </div>
             </div>
-        );
+            </div>
+            </body>
+          
+        )
+      }
     }
-}
 
 export default Portofolio;
